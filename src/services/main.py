d270c2162ab1ba17dad3fbc0dcd5198aa7b40200
Joli Holmes <holmesjoli@gmail.com>
2022-12-03 15:09:00 -0500

    initial data management steps

--- src/services/main.py
+++ src/services/main.py
@@ -1,20 +1,47 @@
 import pandas as pd
 import os
+import numpy as np
+import yaml
+from python.data_management import actors_rename, activities_dm, actors_dm, create_actor_activity_network
+from python.translate import translate_text, authenticate_implicit_with_adc
+
+with open('./config.yaml', 'r', encoding='utf8') as file:
+    config = yaml.safe_load(file)
 
 pth = "../data/"
 raw_pth = os.path.join(pth, "raw")
 processed_pth = os.path.join(pth, "processed")
 
+
+def save_csv(raw_pth):
+
+    for fl in os.listdir(raw_pth):
+
+       # import pdb; pdb.set_trace()
+
+        df =  pd.read_excel(os.path.join(raw_pth, fl))
+
+        name = os.path.splitext(fl)[0]
+
+        df.to_csv(os.path.join(raw_pth, name+'.csv'))
+
 def main():
-    channels = pd.read_excel(os.path.join(raw_pth, "Canali_Richiesta Dati 220913 (CHANNELS) translated.xlsx"))
-    checks = pd.read_excel(os.path.join(raw_pth, "Controlli_Richiesta Dati 220913 (CONTROLLING ACTIVITY)) translated.xlsx"))
-    risks = pd.read_excel(os.path.join(raw_pth, "Rischi_Richiesta Dati 220913 (RISKS) translated.xlsx"))
-    actors = pd.read_excel(os.path.join(raw_pth, "Attori_Richiesta Dati 220913 (ACTORS) translated.xlsx"))
-    applications = pd.read_excel(os.path.join(raw_pth, "Applicativi_Richiesta Dati 220913 (APPLICATIONS) translated.xlsx"))
-    taxonomy = pd.read_excel(os.path.join(raw_pth, "Tassonomia_Richiesta Dati 220913 (TAXONOMY) translated.xlsx"))
-    taxonomy_detailed = pd.read_excel(os.path.join(raw_pth, "TAXONOMY (ENG) 23 febbraio 2022 translated.xlsx"), skiprows = 2)
+
+    data = pd.read_excel(os.path.join(raw_pth, "Richiesta_Dati_V2_EN.xlsb"), sheet_name = "Actors")
+    data = actors_rename(data)
+
+    ## Translate Italian to English
+    authenticate_implicit_with_adc()
+    translate_text(data.actor.unique(), raw_pth, 'actors_translated')
+    translate_text(data.activity.unique(), raw_pth, 'activities_translated')
+
+    activities = activities_dm(data, config, raw_pth)
+    actors = actors_dm(data, config, raw_pth)
+
+    create_actor_activity_network(data, actors, activities)
 
     import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
     main()
+    
---
+++ src/services/python/translate.py
@@ -0,0 +1,103 @@
+from google.cloud import translate, storage
+import os
+import pandas as pd
+from python.helper import write_csv
+import math
+
+project_id = "banca-355515"
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+def authenticate_implicit_with_adc():
+    """
+    When interacting with Google Cloud Client libraries, the library can auto-detect the
+    credentials to use.
+
+    // TODO(Developer):
+    //  1. Before running this sample,
+    //  set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+    //  2. Replace the project variable.
+    //  3. Make sure that the user account or service account that you are using
+    //  has the required permissions. For this sample, you must have "storage.buckets.list".
+    Args:
+        project_id: The project id of your Google Cloud project.
+    """
+
+    # This snippet demonstrates how to list buckets.
+    # *NOTE*: Replace the client created below with the client required for your application.
+    # Note that the credentials are not specified when constructing the client.
+    # Hence, the client library will look for credentials using ADC.
+    storage_client = storage.Client(project=project_id)
+    buckets = storage_client.list_buckets()
+    print("Buckets:")
+    for bucket in buckets:
+        print(bucket.name)
+    print("Listed all storage buckets.")
+
+
+"""
+Yield successive n-sized chunks from lst.
+https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+"""
+def chunks(xs, n):
+    n = max(1, n)
+    return (xs[i:i+n] for i in range(0, len(xs), n))
+
+
+def n_chunks(text):
+
+    n = math.floor(len(text)/400)
+    if len(text)%400 != 0:
+        n = n+1
+    return n
+
+"""
+Translating Italian to English
+Writes out translated text to english and saves as a CSV so the translation does not have to be run each time main is run
+"""
+def translate_text(text, pth, fl_name):
+
+    if(os.path.isfile(os.path.join(pth, fl_name + ".csv")) == False):
+
+        client = translate.TranslationServiceClient()
+        location = "global"
+        parent = f"projects/{project_id}/locations/{location}"
+
+        text = [x for x in text if str(x) != 'nan']
+
+
+        nc = n_chunks(text)
+
+        count = 0
+        englishTranslation = []
+        italianTranslation = []
+
+        for i in range(0, nc):
+
+            if i < nc-1:
+                textsub = text[count:count+400]
+                count = count + 400
+            else:
+                textsub = text[count:len(text)-1]
+
+            response = client.translate_text(
+                request={
+                    "parent": parent,
+                    "contents": textsub,
+                    "mime_type": "text/plain",
+                    "source_language_code": "it",
+                    "target_language_code": "en-US",
+                }
+            )
+
+            trans = []
+
+            for translation in response.translations:
+                trans.append(translation.translated_text)
+
+            englishTranslation = englishTranslation + trans
+            italianTranslation = italianTranslation + textsub
+
+        d = {"Italian": italianTranslation, "English": englishTranslation}
+
+        write_csv(pd.DataFrame(d), pth, fl_name)
