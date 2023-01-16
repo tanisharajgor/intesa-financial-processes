303aec4d273ef1e145ac0a429b01409d5bbf51f2
Joli Holmes <holmesjoli@gmail.com>
2023-01-16 15:01:03 -0500

    added styling to match the other pages

--- src/pages/Network.js
+++ src/pages/Network.js
@@ -1,12 +1,20 @@
+import Main from "../components/Main";
 import Navigation from "../components/Navigation";
 import { StylesProvider } from "@material-ui/core/styles";
+import { useState } from "react";
+
+const id = "circle-packing-chart";
 
 export default function Network() {
+
+    const [riskVariable, updateRiskVariable] = useState("controlTypeMode");
+
     return(
         <StylesProvider injectFirst>
             <div className="Content">
                 <Navigation/>
                 <div className="Query" id="FilterMenu"></div>
+                <Main riskVariable={riskVariable} updateRiskVariable={updateRiskVariable} id={id}/>                
             </div>
         </StylesProvider>
     )
