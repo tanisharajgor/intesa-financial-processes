378ac3db021c7cc62405d8224e3b080c97e80e27
Joli Holmes <holmesjoli@gmail.com>
2023-01-25 20:58:26 -0500

    Revert "updated palette"
    This reverts commit 2d98b13d6c95adb9433cdd824d957d64bc469723.

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,8 +1,7 @@
 import * as d3 from 'd3';
 
 export const palette = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', 
-'#e377c2', '#bcbd22', '#17becf', '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', 
-'#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5'];
+'#e377c2', '#bcbd22', '#ADADAD'];
 
 export const riskVariables = {
     "controlTypeMode": {
