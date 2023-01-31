2de3af12724971dc80bd6a79e6f9c8ff4dc65b0a
Joli Holmes <holmesjoli@gmail.com>
2023-01-31 16:31:34 -0500

    updated sequential scale

--- src/utils/global.js
+++ src/utils/global.js
@@ -27,6 +27,26 @@ export const riskVariables = {
     }
 }
 
+// https://stackoverflow.com/questions/6443990/javascript-calculate-brighter-colour
+function increase_brightness(hex, percent) {
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+       ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
+}
+
 // Creates a colorScales for different types of variables
 // yellow: #FFC41F
 // orange: #FF831D
@@ -35,19 +55,12 @@ export function createColorScale(variable, variableLookup) {
     let t = variableLookup[variable]
     if (variable == "controlPeriodocityMode") {
 
-        var s = d3.scaleSequentialLog(d3.interpolate("purple", "orange"))
+        var s = d3.scaleSequentialLog(d3.interpolate("orange", "purple"))
             .domain(d3.extent(t.values));
 
-            console.log(d3.extent(t.values))
-            console.log(t.labels)
-
-            console.log(s(t.values[7]))
-
-            console.log(t.values.map((d) => s(d)))
-
         const scale = d3.scaleOrdinal()
-            .domain([t.labels])
-            .range(t.values.map((d) => s(d)))
+            .domain(t.labels)
+            .range(t.values.map((d) => s(d)));
 
         return scale
 
