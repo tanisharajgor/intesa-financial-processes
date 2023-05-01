2f8bccfcde10b11e7713be547a39e9993aba57a7
Matthew Blanco <mblanco@recurforever.com>
2023-05-01 10:29:04 -0400

    Render circle packing visualization using PIXI

--- src/pages/CirclePacking.js
+++ src/pages/CirclePacking.js
@@ -3,8 +3,9 @@ import Main from "../components/Main";
 import { createColorScale, applyColorScaleMode, createOpacityScale } from "../utils/global";
 import data from "../data/processed/nested/processes.json";
 import * as d3 from 'd3';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { inspectHierarchySummary } from "../components/Inspect";
+import { CirclePackingDiagram } from "../visualization/circle-packing";
 
 const id = "circle-packing-chart";
 let tooltip;
@@ -75,70 +76,13 @@ export default function CirclePacking() {
     colorScale = createColorScale(viewVariable);
     const opacityScale = createOpacityScale();
 
-    // Draw circle packing once
+    // console.log(root, root.descendants())
+
+    const circlePackingDiagram = useRef(new CirclePackingDiagram(root.descendants().slice(1)))
+
     useEffect(() => {
-
-        const svg = d3.select(`#${id}`).append("svg")
-            .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-            .attr("transform","rotate(-90)")
-            .style("cursor", "pointer")
-            .on("click", (event) => zoom(event, root));
-
-        //tooltip
-        tooltip = d3.select(`#${id}`)
-            .append("div")
-            .attr("class", "tooltip")
-            .style("position", "absolute")
-            .style("left", "0px")
-            .style("top", "0px")
-            .style("visibility", "hidden")
-            .style("padding", "10px")
-            .style("pointer-events", "none")
-            .style("border-radius", "5px")
-            .style("background-color", "rgba(0, 0, 0, 0.65)")
-            .style("font-family", '"Helvetica Neue", Helvetica, Arial, sans-serif')
-            .style("font-weight", "normal")
-            .style("border", "1px solid rgba(78, 81, 85, 0.7)")
-            .style("font-size", "16px");
-    
-        const circle = svg.append("g")
-            .selectAll("circle")
-            .data(root.descendants().slice(1))
-            .join("circle")
-                .attr("fill", d => applyColorScaleMode(d.data, viewVariable, colorScale))
-                .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
-                .on("mouseout", function() { d3.select(this).attr("stroke", null); })
-                .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()))
-                .attr("stroke-width", .5)
-                .attr("stroke", "grey")
-                .attr("fill-opacity", d => opacityScale(d.data.treeLevel))
-                .attr("visibility", d => d.data.treeLevel === 0 ? "hidden": "visible");
-
-        zoomTo([root.x, root.y, root.r * 2]);
-
-        function zoomTo(v) {
-            const k = width / v[2];
-
-            view = v;
-            circle.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-            circle.attr("r", d => d.r * k);
-        }
-
-        function zoom(event, d) {
-            const focus0 = focus;
-
-            focus = d;
-
-            const transition = svg.transition()
-                .duration(event.altKey ? 7500 : 750)
-                .tween("zoom", d => {
-                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-                return t => zoomTo(i(t));
-            });
-        }
-
-        inspectCirclePacking(data, viewVariable, updateViewHoverValue);
-
+        circlePackingDiagram.current.init(id)
+        circlePackingDiagram.current.draw(viewVariable)
     }, [])
 
     // Update the visual aesthetics of the visualization that change with a user input
