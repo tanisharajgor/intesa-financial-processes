461891a286cd06965bc46861741fdc98675eb207
Matthew Blanco <mblanco@recurforever.com>
2023-05-19 16:50:08 +0200

    Add styling for zoom controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -5,18 +5,31 @@ const StyledControls = styled('div')`
     padding: ${props =>  props.theme.padding };
 `
 
-function myFunction() {
-    console.log("hi there")
-}
+const StyledControlButton = styled('button')`
+    background: transparent;
+    border: 1px solid transparent;
+    cursor: pointer;
+
+    &:hover {
+        border-color: ${props =>  props.theme.color.secondary };
+        border-radius: 15%;
+    }
+`
 
 export default function Control({controls}) {
 
     return (
         <StyledControls className="Controls">
             <div className="inner">
-                <button onClick={() => controls.reset()}><img src={process.env.PUBLIC_URL + "/assets/crop_free.svg"}/></button>
-                <button onClick={() => controls.zoomIn()}><img src={process.env.PUBLIC_URL + "/assets/zoom_in.svg"}/></button>
-                <button onClick={() => controls.zoomOut()}><img src={process.env.PUBLIC_URL + "/assets/zoom_out.svg"}/></button>
+                <StyledControlButton onClick={() => controls.reset()}>
+                    <img alt="Button to reset the scale of the visualization" src={process.env.PUBLIC_URL + "/assets/crop_free.svg"}/>
+                </StyledControlButton>
+                <StyledControlButton onClick={() => controls.zoomIn()}>
+                    <img alt="Button to zoom further into the visualization" src={process.env.PUBLIC_URL + "/assets/zoom_in.svg"}/>
+                </StyledControlButton>
+                <StyledControlButton onClick={() => controls.zoomOut()}>
+                    <img alt="Button to zoom out of the visualization" src={process.env.PUBLIC_URL + "/assets/zoom_out.svg"}/>
+                </StyledControlButton>
             </div>
         </StyledControls>
     )
