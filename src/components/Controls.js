39bcd6d08b88ba5fff1b07f3c5df9536556ee6eb
Matthew Blanco <mblanco@recurforever.com>
2023-05-31 02:29:16 +0200

    Add ripple effect to control buttons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Ripple from "./Ripple";
 
 const StyledControls = styled('div')`
     width: ${props =>  props.theme.viewColWidth };
@@ -9,6 +10,7 @@ const StyledControlButton = styled('button')`
     background: transparent;
     border: 1px solid transparent;
     cursor: pointer;
+    position: relative;
 
     &:hover {
         border-color: ${props =>  props.theme.color.secondary };
@@ -27,12 +29,15 @@ export default function Control({hideControls, controls}) {
             <div className="inner">
                 <StyledControlButton onClick={() => controls.reset()}>
                     <img alt="Button to reset the scale of the visualization" src={process.env.PUBLIC_URL + "/assets/crop_free.svg"}/>
+                    <Ripple color={"#FFFFFF"} duration={1000}/>
                 </StyledControlButton>
                 <StyledControlButton onClick={() => controls.zoomIn()}>
                     <img alt="Button to zoom further into the visualization" src={process.env.PUBLIC_URL + "/assets/zoom_in.svg"}/>
+                    <Ripple color={"#FFFFFF"} duration={1000}/>
                 </StyledControlButton>
                 <StyledControlButton onClick={() => controls.zoomOut()}>
                     <img alt="Button to zoom out of the visualization" src={process.env.PUBLIC_URL + "/assets/zoom_out.svg"}/>
+                    <Ripple color={"#FFFFFF"} duration={1000}/>
                 </StyledControlButton>
             </div>
         </StyledControls>
