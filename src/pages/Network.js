c1a0edaa09828a40855bfcd918cc84205c429dda
Joli Holmes <holmesjoli@gmail.com>
2023-06-08 14:59:45 -0400

    restructured code for new data structure

--- src/pages/Network.js
+++ src/pages/Network.js
@@ -3,7 +3,8 @@ import Navigation from "../components/Navigation";
 import FilterProcess from "../components/FilterProcess";
 import FilterType from "../components/FilterType";
 import { useEffect, useRef, useState } from "react";
-import graph from "../data/processed/nested/network2.json";
+import links from "../data/processed/nested/links.json";
+import nodes from "../data/processed/nested/nodes.json";
 import { QueryMenu } from "cfd-react-components";
 import NetworkVisualization from "../visualization/network-visualization";
 import * as Global from "../utils/global";
@@ -11,19 +12,41 @@ import { inspectNetworkSummary } from "../components/Inspect";
 import * as d3 from 'd3';
 
 const id = "network-chart";
-let colorScale;
+
+// Combines the nodes and links into a single object
+function combineNodeLink(selectedLevel3ID, nodes, links) {
+
+    let nodesData = Object.assign({}, nodes.find((d) => d.id === selectedLevel3ID)).nodes;
+    let linksData = Object.assign({}, links.find((d) => d.id === selectedLevel3ID)).links;
+    let dataNew = {id: selectedLevel3ID, nodes: nodesData, links: linksData};
+
+    return dataNew;
+}
+
+// Combines the two types of links into a single array
+function combineLink(data) {
+
+    if (data.links.deve) {
+        data.links.deve.map(d => d.connect_actor_activity = true);
+        data.links.non_deve.map(d => d.connect_actor_activity = false);
+        data.links = data.links.deve.concat(data.links.non_deve);
+    }
+
+    return data;
+}
 
 // Filters the data by level3ID and activity Type
 function filterData(selectedLevel3ID, selectedActivities, selectedActors) {
 
-    let dataNew = Object.assign({}, graph.find((d) => d.id === selectedLevel3ID));
+    let dataNew = combineNodeLink(selectedLevel3ID, nodes, links);
+    combineLink(dataNew);
 
     let actorIdsFiltered = dataNew.nodes.filter(d => d.group === "Actor" && selectedActors.includes(d.type)).map(d => d.id);
     let activityIdsFiltered = dataNew.nodes.filter(d => d.group === "Activity" && selectedActivities.includes(d.type)).map(d => d.id);
 
-    let links = dataNew.links.filter(d => d.source.id === undefined ? actorIdsFiltered.includes(d.source) && activityIdsFiltered.includes(d.target): actorIdsFiltered.includes(d.source.id) && activityIdsFiltered.includes(d.target.id));
-    let actorIds = [...new Set(links.map(d => d.source.id === undefined ? d.source: d.source.id))];
-    let activityIds = [...new Set(links.map(d => d.target.id === undefined ? d.target: d.target.id))];
+    let linksNew = dataNew.links.filter(d => d.source.id === undefined ? actorIdsFiltered.includes(d.source) && activityIdsFiltered.includes(d.target): actorIdsFiltered.includes(d.source.id) && activityIdsFiltered.includes(d.target.id));
+    let actorIds = [...new Set(linksNew.map(d => d.source.id === undefined ? d.source: d.source.id))];
+    let activityIds = [...new Set(linksNew.map(d => d.target.id === undefined ? d.target: d.target.id))];
   
     let riskIds = Global.filterLinksSourceToTarget(dataNew.links, activityIds);
     let controlIds = Global.filterLinksSourceToTarget(dataNew.links, riskIds);
@@ -41,8 +64,13 @@ export default function Network() {
     const [viewVariable, updateViewVariable] = useState("riskType");
 
     // User Input selection
-    const [selectedLevel3ID, updateLevel3ID] = useState(graph[0].id);
-    const [data, updateData] = useState(Object.assign({}, graph.find((d) => d.id === selectedLevel3ID)));
+    const [selectedLevel3ID, updateLevel3ID] = useState(links[0].id);
+
+    let dataNew = combineNodeLink(selectedLevel3ID, nodes, links);
+
+    const [data, updateData] = useState(dataNew);
+
+    combineLink(data);
 
     // Possible set of activities/actors to choose from
     const [possibleActivities, updateActivityType] = useState([...new Set(data.nodes.filter(d => d.group === "Activity").map(d => d.type))]);
@@ -58,9 +86,6 @@ export default function Network() {
 
     // Initiating the network diagram
     const networkDiagram = useRef(new NetworkVisualization(data, updateSymbolHoverValue, updateViewHoverValue));
-
-    // Set-up scales
-    colorScale = Global.createColorScale(viewVariable);
 
     // React Hooks
     useEffect(() => {
