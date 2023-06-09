4cd088be019a1651d226325b75c8ec6f95f836c3
Joli Holmes <holmesjoli@gmail.com>
2023-06-09 00:33:19 -0400

    updated to fix issue with wrong set of types updates

--- src/services/python/nest_data.py
+++ src/services/python/nest_data.py
@@ -156,6 +156,7 @@ def create_sub_processes(df, root1, root2, children = None, tree_level = None):
             d["children"] = subset_list(childrenIDs, children)
             d["viewId"] = "Process"
         else:
+            d["activityType"] = df_sub[df_sub[root1ID] == id].activityType.iloc[0]
             if df_sub[df_sub[root1ID] == id].activityType.iloc[0] == "Control activity":
                 d["viewId"] = "Control activity"
             else:
