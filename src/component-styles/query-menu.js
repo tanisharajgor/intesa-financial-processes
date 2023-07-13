41c93fad501279e6d8cd6f9b5a915be3bf24bd82
Tanisha Rajgor <tanisha.rajgor@gmail.com>
2023-07-13 14:02:41 -0400

    Cosmetic  fixes, drag handle bar, and vertical stretching.

--- src/component-styles/query-menu.js
+++ src/component-styles/query-menu.js
@@ -2,13 +2,29 @@ import { QueryMenu } from "cfd-react-components";
 import styled from "styled-components";
 
 export const Menu = styled.div`
-    overflow: scroll;
+    resize: vertical;
+    overflow-y: scroll;
     height: 65vh;
-    width: 30vw;
+    width: 22vw;
     z-index: +9;
+    padding: 2%;
     position: absolute;
-    left: ${props => props.isFullscreen ? "-30vw;" : "0vw;"};
     top: 15vh;
+    left: ${props => props.isFullscreen ? "-30vw;" : "0vw;"};
     transition: left 0.25s cubic-bezier(0.4, 0, 0.2, 1) 0ms;
     background-color: ${props => props.theme.backgroundColor.main};
 `;
+
+export const DragBar = styled.div`
+    border: 2px solid #1d8693;
+    background-color: #21252a;
+    text-align: center; 
+    cursor: grab;
+    margin-bottom: 2vh;
+`;
+
+export const CollapseButton = styled.div`
+    border: 2px solid #1d8693;
+    background-color: #21252a;
+`;
+
--- src/pages/CirclePacking.js
+++ src/pages/CirclePacking.js
@@ -8,9 +8,10 @@ import { CirclePackingDiagram } from "../visualization/circle-packing-visualizat
 import FilterType from "../components/FilterType";
 import { activityTypeValues } from "../utils/global";
 import Description from "../components/Description";
-import { Menu } from "../component-styles/query-menu";
+import { CollapseButton, DragBar, Menu } from "../component-styles/query-menu";
 import { Content } from "../component-styles/content";
 import Draggable from 'react-draggable';
+import { ChevronButton } from '../component-styles/chevron-button';
 
 const id = "circle-packing-chart";
 
@@ -19,6 +20,9 @@ export default function CirclePacking() {
     const [viewVariable, updateViewVariable] = useState("riskType");
     const [viewHoverValue, updateViewHoverValue] = useState(undefined);
     const [isFullscreen, setFullscreen] = useState(false);
+    const [shouldRotate, setRotate] = useState(false);
+
+    const handleRotate = () => setRotate(!shouldRotate);
 
     // Possible set of activities/actors to choose from
     const possibleActivities = activityTypeValues;
@@ -62,52 +66,59 @@ export default function CirclePacking() {
         // updateViewVariable(updatedView);
     }, [selectedActivities, viewVariable]);
 
+    // useEffect(() => {
+    //     // Make the DIV element draggable:
+    //     const dragElement = (elmnt) => {
+    //         let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    useEffect(() => {
-        // Make the DIV element draggable:
-        const dragElement = (elmnt) => {
-            let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    //         const dragMouseDown = (e) => {
+    //             e = e || window.event;
+    //             e.preventDefault();
+    //             pos3 = e.clientX;
+    //             pos4 = e.clientY;
+    //             document.onmouseup = closeDragElement;
+    //             document.onmousemove = elementDrag;
+    //         };
 
-            const dragMouseDown = (e) => {
-                e = e || window.event;
-                e.preventDefault();
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmouseup = closeDragElement;
-                document.onmousemove = elementDrag;
-            };
+    //         const elementDrag = (e) => {
+    //             e = e || window.event;
+    //             e.preventDefault();
+    //             pos1 = pos3 - e.clientX;
+    //             pos2 = pos4 - e.clientY;
+    //             pos3 = e.clientX;
+    //             pos4 = e.clientY;
+    //             elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    //             elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    //         };
 
-            const elementDrag = (e) => {
-                e = e || window.event;
-                e.preventDefault();
-                pos1 = pos3 - e.clientX;
-                pos2 = pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-            };
+    //         const closeDragElement = () => {
+    //             document.onmouseup = null;
+    //             document.onmousemove = null;
+    //         };
+    //     };
 
-            const closeDragElement = () => {
-                document.onmouseup = null;
-                document.onmousemove = null;
-            };
-        };
-
-        dragElement(document.getElementById("queryMenu"));
-    }, []);
+    //     dragElement(document.getElementById("queryMenu"));
+    // }, []);
 
     return (
         <>
             <Navigation isFullscreen={isFullscreen} />
             <Content>
-                <Draggable>
-                    <Menu className="Query" id="FilterMenu" width={"22rem"}>
-                        <Description>
-                            <h4>Ecosystem</h4>
-                            <p>Click on the circles to zoom into the process visualization.</p>
-                        </Description>
-                        <FilterType typesChecked={selectedActivities} updateSelection={updateActivities} typeValues={possibleActivities} label="Inspect by Activity Type" />
+                <Draggable bounds={{top: '80vh'}} handle="strong">
+                    <Menu className="Query" id="FilterMenu" width={"22rem"}
+                    style={{position: 'absolute', bottom: '50px', left: '20px'}}>
+                        <div className="box no-cursor">
+                            <strong className="cursor">
+                                <DragBar>Drag Me</DragBar>
+                            </strong>
+                            <ChevronButton shouldRotate={shouldRotate} onClick={handleRotate}>
+                            </ChevronButton>
+                            <Description>
+                                <h4>Ecosystem</h4>
+                                <p>Click on the circles to zoom into the process visualization.</p>
+                            </Description>
+                            <FilterType typesChecked={selectedActivities} updateSelection={updateActivities} typeValues={possibleActivities} label="Inspect by Activity Type" />
+                        </div>
                     </Menu>
                 </Draggable>
                 <Main
