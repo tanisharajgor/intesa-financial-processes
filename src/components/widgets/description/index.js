1c64802bf29e1d289151fe5aa4ae64f04d91c08d
Matt Blanco <mattmblanco@gmail.com>
2023-07-27 08:51:18 -0400

    fix component merge conflicts

--- src/components/widgets/description/index.js
+++ src/components/widgets/description/index.js
@@ -1,8 +1,19 @@
-// Styles
-import { StyledDescription } from "./style";
+// Libraries
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export function Description(props) {
-    return (
-        <StyledDescription>{ props.children }</StyledDescription>
-    )
+// Styles
+import { StyledDescription } from '../component-styles/description';
+
+// Prop types
+Description.propTypes = {
+  children: PropTypes.string
+};
+
+export default function Description ({children}) {
+  return (
+    <StyledDescription>
+      <p>{ children }</p>
+    </StyledDescription>
+  );
 }
