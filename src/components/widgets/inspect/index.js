3e7a472e9b3b2b41cd684b126bdb89a870d14a70
Joli Holmes <holmesjoli@gmail.com>
2023-07-30 16:09:02 -0400

    removed imports which are not used in this pr

--- src/components/widgets/inspect/index.js
+++ src/components/widgets/inspect/index.js
@@ -2,10 +2,6 @@
 import React from 'react';
 import * as d3 from 'd3';
 import { viewVariables, createLabelScale } from '../../../utils/global';
-// import * as Theme from '../../../utils/theme';
-
-// Libraries
-import '@pixi/graphics-extras';
 
 const treeLevelScale = d3.scaleOrdinal()
   .domain([0, 1, 2, 3, 4])
